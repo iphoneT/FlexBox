a3e5f85948d780ab2e77f5a8cb06dbe73faec653
Vaibhav <admin@Admin-2.local>
2021-01-09 16:10:18 +0530

    130

--- App.js
+++ App.js
@@ -15,7 +15,7 @@ class App extends React.Component {
     return (
       <View style={{ flexWrap: 'wrap',flex: 1, justifyContent:'space-between',flexDirection:'column' }}>
         <View style={{ width:130,height:130, backgroundColor:'red'}} ></View>
-        <View style={{ width:120,height:120, backgroundColor:'orange'}} ></View>
+        <View style={{ width:130,height:130, backgroundColor:'orange'}} ></View>
         <View style={{ width:120,height:120, backgroundColor:'blue'}} ></View>
       </View>
     )
