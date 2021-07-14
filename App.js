dd09ab349b22c637995a71ab2f344c60d23a7603
iphoneT <iphone@trivediinfotech.com>
2021-07-14 17:02:59 +0530

    w
    Signed-off-by: iphoneT <iphone@trivediinfotech.com>

--- App.js
+++ App.js
@@ -12,7 +12,6 @@ import {View, Button, StyleSheet, Text} from 'react-native';
 class App extends React.Component {
   render() {
     return (
-<<<<<<< HEAD
       <View
         style={{
           flexWrap: 'wrap',
@@ -20,17 +19,10 @@ class App extends React.Component {
           justifyContent: 'space-between',
           flexDirection: 'column',
         }}>
-        <Text>App JS is Great1</Text>
         <View style={{width: 150, height: 130, backgroundColor: 'red'}}></View>
         <View
           style={{width: 130, height: 130, backgroundColor: 'orange'}}></View>
         <View style={{width: 200, height: 200, backgroundColor: 'blue'}}></View>
-=======
-      <View style={{ flexWrap: 'wrap',flex: 1, justifyContent:'space-between',flexDirection:'column' }}>
-        <View style={{ width:150,height:130, backgroundColor:'red'}} ></View>
-        <View style={{ width:130,height:130, backgroundColor:'orange'}} ></View>
-        <View style={{ width:200,height:200, backgroundColor:'blue'}} ></View>
->>>>>>> b720b52d72c28ae8436d079c59ae534a01cd5c7d
       </View>
     );
   }
