12f30a5e18aec5bf39679c4b466eed9f4afc2c4e
iphoneT <iphone@trivediinfotech.com>
2021-07-14 15:34:44 +0530

    w
    Signed-off-by: iphoneT <iphone@trivediinfotech.com>

--- FromWindows.js
+++ FromWindows.js
@@ -5,7 +5,7 @@ class FromWindows extends React.Component {
   render() {
     return (
       <View>
-        <Text>From Windows testing 1</Text>
+        <Text>From Windows testing hello</Text>
       </View>
     );
   }
