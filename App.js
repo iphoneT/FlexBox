92d1172e2d22c9a4e625d8e0f78f208d7117be07
iphoneT <iphone@trivediinfotech.com>
2021-07-14 17:21:57 +0530

    w
    Signed-off-by: iphoneT <iphone@trivediinfotech.com>

--- App.js
+++ App.js
@@ -19,9 +19,8 @@ class App extends React.Component {
           justifyContent: 'space-between',
           flexDirection: 'column',
         }}>
-<<<<<<< HEAD
         <View style={{width: 150, height: 130, backgroundColor: 'red'}} />
-        <View style={{width: 130, height: 130, backgroundColor: 'yellow'}}>
+        <View style={{width: 130, height: 140, backgroundColor: 'yellow'}}>
           <Image
             source={require('./images/a.jpeg')}
             style={{
@@ -33,15 +32,6 @@ class App extends React.Component {
           />
         </View>
         <View style={{width: 200, height: 200, backgroundColor: 'blue'}}></View>
-=======
-        <View style={{width: 150, height: 130, backgroundColor: 'red'}}></View>
-        <View
-          style={{width: 130, height: 130, backgroundColor: 'yellow'}}>
-          </View>
-        <View style={{width: 200, height: 200, backgroundColor: 'blue'}}>
-        <Image source={require('./images/b.jpg')} />
-        </View>
->>>>>>> ee36d5a3a84dc69703c1e66ebbe4a69f1fa9a95d
       </View>
     );
   }
