6e6bd00fb92caf52662ed9d7b7ff8a4a4b8e4d1d
iphoneT <iphone@trivediinfotech.com>
2021-07-14 13:20:18 +0530

    windows
    Signed-off-by: iphoneT <iphone@trivediinfotech.com>

---
+++ FromWindows.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+class FromWindows extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>From Windows testing</Text>
+      </View>
+    );
+  }
+}
