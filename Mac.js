df7ddbf82437b35634dcc6d0743111f343c8d1e3
iphone T <iphone@trivediinfotech.com>
2021-07-14 15:27:04 +0530

    3
    Signed-off-by: iphone T <iphone@trivediinfotech.com>

--- Mac.js
+++ Mac.js
@@ -6,19 +6,7 @@ class Mac extends React.Component {
   render() {
     return (
       <View style={{ flexWrap: 'wrap',flex: 1, justifyContent:'space-between',flexDirection:'column' }}>
-<<<<<<< HEAD
         <Text>From Mac testing 111</Text>
-=======
-<<<<<<< HEAD
-        <Text>From Mac testing 11111</Text>
-=======
-<<<<<<< HEAD
-        <Text>From Mac testing 4567890</Text>
-=======
-        <Text>From Mac testing 111</Text>
->>>>>>> 1a1d33afa6db5bcdc8ea491f2fc835bf8f3149a3
->>>>>>> 91a743c448bed9687ed18d138ce1097f141570db
->>>>>>> 89a9bf636a51c950ea67abcb1071c695bb163f55
       </View>
     )
   }
