198b9136e5fc60525f316dab066a04242b4f2d97
iphoneT <iphone@trivediinfotech.com>
2021-07-14 17:20:04 +0530

    w
    Signed-off-by: iphoneT <iphone@trivediinfotech.com>

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
  */
 
 import React from 'react';
-import {View, Button, StyleSheet, Text} from 'react-native';
+import {View, Button, StyleSheet, Text, Image} from 'react-native';
 
 class App extends React.Component {
   render() {
@@ -19,9 +19,18 @@ class App extends React.Component {
           justifyContent: 'space-between',
           flexDirection: 'column',
         }}>
-        <View style={{width: 150, height: 130, backgroundColor: 'red'}}></View>
-        <View
-          style={{width: 130, height: 130, backgroundColor: 'yellow'}}></View>
+        <View style={{width: 150, height: 130, backgroundColor: 'red'}} />
+        <View style={{width: 130, height: 130, backgroundColor: 'yellow'}}>
+          <Image
+            source={require('./images/a.jpeg')}
+            style={{
+              width: 120,
+              height: 110,
+              alignSelf: 'center',
+              resizeMode: 'center',
+            }}
+          />
+        </View>
         <View style={{width: 200, height: 200, backgroundColor: 'blue'}}></View>
       </View>
     );
